8de0905345148fe8f26713f0f7a5a58b857ffdbb
写代码的厨子 <465351405@qq.com>
2021-02-20 10:16:46 +0800

    增加OverlayPanel

---
+++ CookPopularControl/Controls/OverlayPanel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+/*
+ * Copyright (c) 2021 All Rights Reserved.
+ * Description：$Do something$ 
+ * Author： Chance_写代码的厨子
+ * Create Time：2021-02-20 10:14:22
+ */
+namespace CookPopularControl.Controls
+{
+    /// <summary>
+    /// 重叠面板
+    /// </summary>
+    /// <remarks>用于代替Grid</remarks>
+    public class OverlayPanel : Panel
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var size = new Size();
+
+            foreach (UIElement child in InternalChildren)
+            {
+                if (child != null)
+                {
+                    child.Measure(availableSize);
+                    size.Width = Math.Max(size.Width, child.DesiredSize.Width);
+                    size.Height = Math.Max(size.Height, child.DesiredSize.Height);
+                }
+            }
+
+            return size;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            foreach (UIElement child in InternalChildren)
+            {
+                child?.Arrange(new Rect(finalSize));
+            }
+
+            return finalSize;
+        }
+    }
+}
