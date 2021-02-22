4a1955092a5d1f2daaf4a185bc0d0773e56ed0d4
写代码的厨子 <465351405@qq.com>
2021-02-22 16:58:53 +0800

    buttonbase ripple test

--- TestDemo/MainWindow.xaml.cs
+++ TestDemo/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace TestDemo
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : ChromeWindow
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
