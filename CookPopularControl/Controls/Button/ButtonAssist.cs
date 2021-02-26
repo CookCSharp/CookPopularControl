4da1609376150b86fe4bf75e367a89a05d3d5607
写代码的厨子 <465351405@qq.com>
2021-02-26 18:09:39 +0800

    增加GifStream的实现

--- CookPopularControl/Controls/Button/ButtonAssist.cs
+++ CookPopularControl/Controls/Button/ButtonAssist.cs
@@ -2,6 +2,7 @@
 using CookPopularControl.Tools.Boxes;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,14 @@ namespace CookPopularControl.Controls.Button
         public static void SetGifSource(DependencyObject obj, Uri value) => obj.SetValue(GifSourceProperty, value);
         public static readonly DependencyProperty GifSourceProperty =
             DependencyProperty.RegisterAttached("GifSource", typeof(Uri), typeof(ButtonAssist), new PropertyMetadata(default(Uri)));
+
+        public static Stream GetGifStream(DependencyObject obj) => (Stream)obj.GetValue(GifStreamProperty);
+        public static void SetGifStream(DependencyObject obj, Stream value) => obj.SetValue(GifStreamProperty, value);
+        /// <summary>
+        /// 提供<see cref="Stream"/>的Gif图像
+        /// </summary>
+        public static readonly DependencyProperty GifStreamProperty =
+            DependencyProperty.RegisterAttached("GifStream", typeof(Stream), typeof(ButtonAssist), new PropertyMetadata(default(Stream)));
 
         public static double GetGifWidth(DependencyObject obj) => (double)obj.GetValue(GifWidthProperty);
         public static void SetGifWidth(DependencyObject obj, double value) => obj.SetValue(GifWidthProperty, value);
