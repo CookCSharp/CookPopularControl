78a7a36d9d5762515d838654eb79e40f96f33e85
写代码的厨子 <465351405@qq.com>
2021-03-11 17:54:38 +0800

    Add LinearProgressBar Control

---
+++ CookPopularControl/Controls/Progress/ProgressBarAssistant.cs
@@ -0,0 +1,54 @@
+﻿using CookPopularControl.Tools.Boxes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+
+
+/*
+ * Copyright (c) 2021 All Rights Reserved.
+ * Description：ProgressBarAssistant
+ * Author： Chance_写代码的厨子
+ * Create Time：2021-03-11 15:32:24
+ */
+namespace CookPopularControl.Controls.Progress
+{
+    /// <summary>
+    /// 表示<see cref="ProgressBar"/>的辅助类
+    /// </summary>
+    public class ProgressBarAssistant
+    {
+        public static bool GetIsShowPercent(DependencyObject obj) => (bool)obj.GetValue(IsShowPercentProperty);
+        public static void SetIsShowPercent(DependencyObject obj, bool value) => obj.SetValue(IsShowPercentProperty, ValueBoxes.BooleanBox(value));
+        /// <summary>
+        /// <see cref="IsShowPercentProperty"/>提供是否显示百分比的附加属性
+        /// </summary>
+        public static readonly DependencyProperty IsShowPercentProperty =
+            DependencyProperty.RegisterAttached("IsShowPercent", typeof(bool), typeof(ProgressBarAssistant),
+                    new FrameworkPropertyMetadata(ValueBoxes.FalseBox, FrameworkPropertyMetadataOptions.Inherits, OnPropertiesValueChanged));
+
+        public static Brush GetTextColor(DependencyObject obj) => (Brush)obj.GetValue(TextColorProperty);
+        public static void SetTextColor(DependencyObject obj, Brush value) => obj.SetValue(TextColorProperty, value);
+        /// <summary>
+        /// <see cref="TextColorProperty"/>提供文本颜色的附加属性
+        /// </summary>
+        public static readonly DependencyProperty TextColorProperty =
+            DependencyProperty.RegisterAttached("TextColor", typeof(Brush), typeof(ProgressBarAssistant),
+                new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.Inherits, OnPropertiesValueChanged));
+
+
+        private static void OnPropertiesValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var progress = d as ProgressBar;
+            if (progress != null)
+            {
+                progress.UpdateLayout();
+            }
+        }
+    }
+}
