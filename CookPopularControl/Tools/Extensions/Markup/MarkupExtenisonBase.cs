b4095bc2f15689e5a5b7fdb42b5d59509f409295
写代码的厨子 <465351405@qq.com>
2021-03-11 14:34:49 +0800

    Add MarkupExtensionBase

---
+++ CookPopularControl/Tools/Extensions/Markup/MarkupExtenisonBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+
+
+/*
+ * Copyright (c) 2021 All Rights Reserved.
+ * Description：MarkupExtenisonBase
+ * Author： Chance_写代码的厨子
+ * Create Time：2021-03-11 13:51:52
+ */
+namespace CookPopularControl.Tools.Extensions.Markup
+{
+    /// <summary>
+    /// 为可以由 .NET Framework XAML 服务及其他 XAML 读取器和 XAML 编写器支持的 XAML 标记扩展实现提供基类
+    /// </summary>
+    public abstract class MarkupExtenisonBase : MarkupExtension
+    {
+        protected MarkupExtenisonBase()
+        {
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {           
+            return this;
+        }
+    }
+}
---
+++ CookPopularControl/Tools/Extensions/Markup/MarkupExtensionBase`T.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+
+
+/*
+ * Copyright (c) 2021 All Rights Reserved.
+ * Description：MarkupExtensionBase_T
+ * Author： Chance_写代码的厨子
+ * Create Time：2021-03-11 14:00:53
+ */
+namespace CookPopularControl.Tools.Extensions.Markup
+{
+    public abstract class MarkupExtensionBase<T> : MarkupExtenisonBase
+    {
+        private readonly object parameter;
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.RegisterAttached("Value", typeof(T), typeof(MarkupExtenisonBase), new PropertyMetadata(default(T)));
+
+        protected MarkupExtensionBase(object _parameter)
+        {
+            this.parameter = _parameter;
+        }
+
+        /// <summary>
+        /// 获取绑定目标的属性值
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        protected bool TryGetValue(IServiceProvider serviceProvider, out T value)
+        {
+            if (parameter is BindingBase binding)
+            {
+                if (serviceProvider is IProvideValueTarget provideValueTarget)
+                {
+                    if (provideValueTarget.TargetObject is DependencyObject target)
+                    {
+                        try
+                        {
+                            BindingOperations.SetBinding(target, ValueProperty, binding);
+                            value = (T)target.GetValue(ValueProperty);
+
+                            return true;
+                        }
+                        catch (Exception)
+                        {
+                            value = default(T);
+                            return false;
+                        }
+                        finally
+                        {
+                            BindingOperations.ClearBinding(target, ValueProperty);
+                        }
+                    }
+                }
+            }
+
+            value = default(T);
+            return false;
+        }
+    }
+}
