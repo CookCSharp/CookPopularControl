6f17ecd617164befbfc65475127f95cf80d4791f
写代码的厨子 <465351405@qq.com>
2021-03-11 15:25:32 +0800

    Test

--- TestDemo/Demos/ButtonDemo.xaml.cs
+++ TestDemo/Demos/ButtonDemo.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +21,42 @@ namespace TestDemo.Demos
     /// <summary>
     /// ButtonDemo.xaml 的交互逻辑
     /// </summary>
-    public partial class ButtonDemo : UserControl
+    //[AddINotifyPropertyChangedInterface]
+    public partial class ButtonDemo : UserControl, INotifyPropertyChanged
     {
         public ButtonDemo()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    Value = i;
+                    System.Threading.Thread.Sleep(100);
+                }
+            });
+        }
+
+        private double _value;
+        public double Value 
+        {
+            get => _value;
+            set
+            {
+                if (_value == value) return;
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
