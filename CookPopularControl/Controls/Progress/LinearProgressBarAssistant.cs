59ce2928208c5f189b5b103ad0a23b1087cc6e3b
写代码的厨子 <465351405@qq.com>
2021-03-16 14:08:30 +0800

    LinearProgressBar 优化

--- CookPopularControl/Controls/Progress/LinearProgressBarAssistant.cs
+++ CookPopularControl/Controls/Progress/LinearProgressBarAssistant.cs
@@ -15,16 +15,19 @@ using System.Windows.Shapes;
 namespace CookPopularControl.Controls.Progress
 {
     /// <summary>
-    /// 表示<see cref="ProgressBar"/>的辅助类
+    /// 表示所有<see cref="ProgressBar"/>的附加属性基类
     /// </summary>
+    /// <remarks>即以LinearProgressBar为基准</remarks>
     [TemplatePart(Name = PathGrid, Type = typeof(Grid))]
     [TemplatePart(Name = PathProgress, Type = typeof(Path))]
     [TemplatePart(Name = Content, Type = typeof(TextBlock))]
+    [TemplatePart(Name = ElementBackground, Type = typeof(Ellipse))]
     public class LinearProgressBarAssistant
     {
         private const string PathProgress = "PART_PGOGRESS";
         private const string PathGrid = "PathGrid";
         private const string Content = "PART_CONTENT";
+        private const string ElementBackground = "PART_Background";
 
         public static bool GetIsShowPercent(DependencyObject obj) => (bool)obj.GetValue(IsShowPercentProperty);
         public static void SetIsShowPercent(DependencyObject obj, bool value) => obj.SetValue(IsShowPercentProperty, ValueBoxes.BooleanBox(value));
@@ -33,7 +36,7 @@ namespace CookPopularControl.Controls.Progress
         /// </summary>
         public static readonly DependencyProperty IsShowPercentProperty =
             DependencyProperty.RegisterAttached("IsShowPercent", typeof(bool), typeof(LinearProgressBarAssistant),
-                    new FrameworkPropertyMetadata(ValueBoxes.FalseBox, FrameworkPropertyMetadataOptions.Inherits, OnPropertiesValueChanged));
+                    new FrameworkPropertyMetadata(ValueBoxes.FalseBox, FrameworkPropertyMetadataOptions.Inherits));
 
         public static Brush GetTextColor(DependencyObject obj) => (Brush)obj.GetValue(TextColorProperty);
         public static void SetTextColor(DependencyObject obj, Brush value) => obj.SetValue(TextColorProperty, value);
@@ -42,16 +45,24 @@ namespace CookPopularControl.Controls.Progress
         /// </summary>
         public static readonly DependencyProperty TextColorProperty =
             DependencyProperty.RegisterAttached("TextColor", typeof(Brush), typeof(LinearProgressBarAssistant),
-                new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.Inherits, OnPropertiesValueChanged));
+                new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.Inherits));
 
+        public static bool GetIsShowIcon(DependencyObject obj) => (bool)obj.GetValue(IsShowIconProperty);
+        public static void SetIsShowIcon(DependencyObject obj, bool value) => obj.SetValue(IsShowIconProperty, ValueBoxes.BooleanBox(value));
+        /// <summary>
+        /// <see cref="IsShowIconProperty"/>提供是否添加图标的附加属性
+        /// </summary>
+        public static readonly DependencyProperty IsShowIconProperty =
+            DependencyProperty.RegisterAttached("IsShowIcon", typeof(bool), typeof(LinearProgressBarAssistant), 
+                new FrameworkPropertyMetadata(ValueBoxes.FalseBox,FrameworkPropertyMetadataOptions.Inherits));
 
-        private static void OnPropertiesValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var progress = d as ProgressBar;
-            if (progress != null)
-            {
-                progress.UpdateLayout();
-            }
-        }
+        public static double GetIconSize(DependencyObject obj) => (double)obj.GetValue(IconSizeProperty);
+        public static void SetIconSize(DependencyObject obj, double value) => obj.SetValue(IconSizeProperty, value);
+        /// <summary>
+        /// <see cref="IconSizeProperty"/>提供图标大小的附加属性
+        /// </summary>
+        public static readonly DependencyProperty IconSizeProperty =
+            DependencyProperty.RegisterAttached("IconSize", typeof(double), typeof(LinearProgressBarAssistant),
+                new FrameworkPropertyMetadata(ValueBoxes.Double30Box, FrameworkPropertyMetadataOptions.Inherits));
     }
 }
---
+++ CookPopularControl/Tools/Windows/Converters/ValueCompareConverters.cs
@@ -0,0 +1,40 @@
+﻿using CookPopularControl.Tools.Boxes;
+using CookPopularControl.Tools.Extensions.Markup;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+
+
+/*
+ * Copyright (c) 2021 All Rights Reserved.
+ * Description：ValueCompareConverters
+ * Author： Chance_写代码的厨子
+ * Create Time：2021-03-16 13:53:10
+ */
+namespace CookPopularControl.Tools.Windows.Converters
+{
+    [MarkupExtensionReturnType(typeof(bool))]
+    public class ValueBetweenZeroAnd100 : MarkupExtensionBase, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double v)
+            {
+                return v > 0 && v <= 100 ? ValueBoxes.TrueBox : ValueBoxes.FalseBox;
+            }
+
+            return ValueBoxes.FalseBox;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
