85e862a89f1240168b42c95ede46a672e87dd759
写代码的厨子 <465351405@qq.com>
2021-03-16 14:19:29 +0800

    Demo Test

--- TestDemo/Demos/AnimationDemo.xaml.cs
+++ TestDemo/Demos/AnimationDemo.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,19 @@ namespace TestDemo.Demos
         public AnimationDemo()
         {
             InitializeComponent();
+        }
+    }
+
+    public class ConverterY : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return - (double)value / 100D;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
