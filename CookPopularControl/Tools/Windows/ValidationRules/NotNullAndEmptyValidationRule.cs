1e629dae2dea769055996c908b6dc7bba43b9bd5
写代码的厨子 <465351405@qq.com>
2021-03-22 09:35:54 +0800

    Add NotEmptyValidationRule

---
+++ CookPopularControl/Tools/Windows/ValidationRules/NotNullAndEmptyValidationRule.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+
+
+/*
+ * Copyright (c) 2021 All Rights Reserved.
+ * Description：NotNullAndEmptyValidationRule
+ * Author： Chance_写代码的厨子
+ * Create Time：2021-03-22 09:01:58
+ */
+namespace CookPopularControl.Tools.Windows.ValidationRules
+{
+    /// <summary>
+    /// NullOrEmpty Rule
+    /// </summary>
+    public class NotNullAndEmptyValidationRule : ValidationRuleBase
+    {
+        private string _errorMessage = "不能为空";
+        public override string ErrorMessage { get => _errorMessage; set => _errorMessage = value; }
+
+        public override ValidationResult ValidateBase(object value, CultureInfo cultureInfo)
+        {
+            return string.IsNullOrEmpty((value ?? "").ToString())
+                    ? new ValidationResult(false, ErrorMessage)
+                    : ValidationResult.ValidResult;
+        }      
+    }
+
+    /// <summary>
+    /// NullOrWhiteSpace Rule
+    /// </summary>
+    public class NotNullAndWhiteSpaceValidationRule : ValidationRuleBase
+    {
+        private string _errorMessage = "不能为空和空格";
+        public override string ErrorMessage { get => _errorMessage; set => _errorMessage = value; }
+
+        public override ValidationResult ValidateBase(object value, CultureInfo cultureInfo)
+        {
+            return string.IsNullOrWhiteSpace((value ?? "").ToString())
+                    ? new ValidationResult(false, ErrorMessage)
+                    : ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ CookPopularControl/Tools/Windows/ValidationRules/ValidationRuleBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+
+
+/*
+ * Copyright (c) 2021 All Rights Reserved.
+ * Description：ValidationRuleBase
+ * Author： Chance_写代码的厨子
+ * Create Time：2021-03-22 09:04:36
+ */
+namespace CookPopularControl.Tools.Windows.ValidationRules
+{
+    /// <summary>
+    /// 提供数据验证基类
+    /// </summary>
+    public abstract class ValidationRuleBase : ValidationRule
+    {
+        /// <summary>
+        /// 错误提示信息
+        /// </summary>
+        public abstract string ErrorMessage { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return ValidateBase(value, cultureInfo);
+        }
+
+        /// <summary>
+        /// 自定义验证<see cref="ValidationResult"/>方法基类
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns></returns>
+        public abstract ValidationResult ValidateBase(object value, CultureInfo cultureInfo);
+    }
+}
