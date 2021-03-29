b458046c461cdc7fd7cd4961cb6e4f358d2f5829
写代码的厨子 <465351405@qq.com>
2021-03-29 14:04:20 +0800

    Test Slider Demo

--- TestDemo/Demos/SliderDemo.xaml.cs
+++ TestDemo/Demos/SliderDemo.xaml.cs
@@ -16,21 +16,13 @@ using System.Windows.Shapes;
 namespace TestDemo.Demos
 {
     /// <summary>
-    /// PopupDemo.xaml 的交互逻辑
+    /// SliderDemo.xaml 的交互逻辑
     /// </summary>
-    public partial class PopupDemo : UserControl
+    public partial class SliderDemo : UserControl
     {
-        public PopupDemo()
+        public SliderDemo()
         {
             InitializeComponent();
-
-            this.Loaded += PopupDemo_Loaded;
-        }
-
-        private void PopupDemo_Loaded(object sender, RoutedEventArgs e)
-        {
-            ExtendPop.IsOpen = true;
-            ExtendPop.StaysOpen = true;
         }
     }
 }
