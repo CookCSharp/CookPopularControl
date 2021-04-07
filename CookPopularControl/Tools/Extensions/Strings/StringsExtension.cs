9335dd0418de226cf8a252fab18bf664555f1d4b
写代码的厨子 <465351405@qq.com>
2021-04-07 12:38:27 +0800

    Add Menu And ContextMenu

---
+++ CookPopularControl/Tools/Extensions/Strings/StringsExtension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+
+
+/*
+ * Copyright (c) 2021 All Rights Reserved.
+ * Description：StringsExtension
+ * Author： Chance_写代码的厨子
+ * Create Time：2021-04-07 11:13:31
+ */
+namespace CookPopularControl.Tools.Extensions.Strings
+{
+    /// <summary>
+    /// 提供<see cref="string"/>的扩展方法
+    /// </summary>
+    public static class StringsExtension
+    {
+        /// <summary>
+        /// 打开链接
+        /// </summary>
+        /// <param name="url"></param>
+        public static void ToOpenLink(this string url)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+        }
+
+        /// <summary>
+        /// 十六进制转<see cref="Brush"/>
+        /// </summary>
+        /// <param name="hexadecimal"></param>
+        /// <returns></returns>
+        public static Brush ToBrush(this string hexadecimal) => (Brush)new BrushConverter().ConvertFromString(hexadecimal);
+    }
+}
---
+++ CookPopularControl/Tools/Windows/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,38 @@
+﻿using CookPopularControl.Tools.Extensions.Markup;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+
+
+/*
+ * Copyright (c) 2021 All Rights Reserved.
+ * Description：StringToVisibilityConverter
+ * Author： Chance_写代码的厨子
+ * Create Time：2021-04-07 10:30:32
+ */
+namespace CookPopularControl.Tools.Windows.Converters
+{
+    [MarkupExtensionReturnType(typeof(Visibility))]
+    [Localizability(LocalizationCategory.NeverLocalize)]
+    public class StringToVisibilityConverter: MarkupExtensionBase, IValueConverter
+    {
+        public Visibility IsEmptyValue { get; set; } = Visibility.Collapsed;
+        public Visibility IsNotEmptyValue { get; set; } = Visibility.Visible;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty((value ?? "").ToString()) ? IsEmptyValue : IsNotEmptyValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
