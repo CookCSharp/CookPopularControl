b8fd890af5882c22d0a8a4c9f0d42861b0c2418b
写代码的厨子 <465351405@qq.com>
2021-05-26 14:14:25 +0800

    some changes

--- CookPopularControl/Communal/ViewModel/ViewModelBase.cs
+++ CookPopularControl/Communal/ViewModel/ViewModelBase.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 
 
@@ -33,5 +35,93 @@ namespace CookPopularControl.Communal.ViewModel
                 OnPropertyChanged(propertyName);
             }
         }
+    }
+
+    internal class DelegateCommand : ICommand
+    {
+        Action _executeMethod;
+        Func<bool> _canExecuteMethod;
+
+        public DelegateCommand(Action executeMethod) : this(executeMethod, () => true)
+        {
+
+        }
+
+        public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            if (executeMethod == null || canExecuteMethod == null)
+                throw new ArgumentNullException($"{nameof(executeMethod)}or{nameof(canExecuteMethod)}");
+
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteMethod();
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeMethod();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Refresh() => CommandManager.InvalidateRequerySuggested();
+    }
+
+    internal class DelegateCommand<T> : ICommand
+    {
+        Action<T> _executeMethod;
+        Func<T, bool> _canExecuteMethod;
+
+        public DelegateCommand(Action<T> executeMethod) : this(executeMethod, t => true)
+        {
+
+        }
+
+        public DelegateCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+        {
+            if (executeMethod == null || canExecuteMethod == null)
+                throw new ArgumentNullException($"{nameof(executeMethod)}or{nameof(canExecuteMethod)}");
+
+            _executeMethod = executeMethod;
+            _canExecuteMethod = canExecuteMethod;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteMethod((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeMethod((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Refresh() => CommandManager.InvalidateRequerySuggested();
     }
 }
