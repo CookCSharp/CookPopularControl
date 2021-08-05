63143417e185da8eb16868b3ead29ff553253fc4
写代码的厨子 <465351405@qq.com>
2021-08-05 11:31:08 +0800

    Simple Test

--- MvvmTestDemo/UserControls/OverViewDemo.xaml.cs
+++ MvvmTestDemo/UserControls/OverViewDemo.xaml.cs
@@ -24,5 +24,29 @@ namespace MvvmTestDemo.UserControls
         {
             InitializeComponent();
         }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            //var s = e.Source;
+            //var s1 = e.OriginalSource;
+            //var tree = sender as TreeView;
+        }
+
+        private void TreeView_Selected(object sender, RoutedEventArgs e)
+        {
+            var item = e.OriginalSource as TreeViewItem;
+            //var s = item.Items.Count;
+            //System.Diagnostics.Debug.WriteLine("111");
+        }
+
+        private void TreeView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            //System.Diagnostics.Debug.WriteLine("222");
+        }
+
+        private void TreeView_Expanded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
