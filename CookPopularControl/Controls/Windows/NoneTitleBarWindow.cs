96bd935fb1057cb4b7b7f95965f69289d2aeca64
写代码的厨子 <465351405@qq.com>
2021-09-27 17:28:44 +0800

    Add NoneTitleBarWindow

---
+++ CookPopularControl/Controls/Windows/NoneTitleBarWindow.cs
@@ -0,0 +1,45 @@
+﻿using CookPopularControl.Tools.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+
+
+/*
+ * Copyright (c) 2021 All Rights Reserved.
+ * Description：NoneTitleBarWindow
+ * Author： Chance_写代码的厨子
+ * Create Time：2021-09-27 16:38:22
+ */
+namespace CookPopularControl.Controls
+{
+    /// <summary>
+    /// <see cref="NoneTitleBarWindow"/>标识没有标题的窗体
+    /// </summary>
+    public class NoneTitleBarWindow : NormalWindow
+    {
+        public static readonly RoutedCommand MoveWindowCommand = new RoutedCommand(nameof(MoveWindowCommand), typeof(NoneTitleBarWindow));
+
+        static NoneTitleBarWindow()
+        {
+            //DefaultStyleKeyProperty.OverrideMetadata(typeof(NoneTitleBarWindow), new FrameworkPropertyMetadata(typeof(NoneTitleBarWindow)));
+            StyleProperty.AddOwner(typeof(NoneTitleBarWindow), new FrameworkPropertyMetadata(default, (s, e) => ResourceHelper.GetResource<Style>("NoneTitleBarWindowStyle")));
+            CommandManager.RegisterClassCommandBinding(typeof(NoneTitleBarWindow), new CommandBinding(MoveWindowCommand, Executed));
+        }
+
+        private static void Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (sender is NoneTitleBarWindow window)
+            {
+                if (e.Command == MoveWindowCommand)
+                {
+                    window.DragMove();
+                }
+            }
+        }
+    }
+}
