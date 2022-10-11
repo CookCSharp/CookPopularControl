53d828891c9b397fc11b5e527a25fdc1a59568d3
写代码的厨子 <465351405@qq.com>
2022-10-11 16:41:55 +0800

    update nuget-push.yml

---
+++ CookPopularControl/Controls/Fields/IHeader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * Description：IHeader 
+ * Author： Chance.Zheng
+ * Create Time: 2022-06-20 17:34:35
+ * .Net Version: 4.6
+ * CLR Version: 4.0.30319.42000
+ * Copyright (c) CookCSharp 2020-2022 All Rights Reserved.
+ */
+namespace CookPopularControl.Controls
+{
+    public interface IHeader
+    {
+        object Header { get; set; }
+    }
+}
---
+++ Version.md
@@ -0,0 +1,16 @@
+# **Version 1.0.2**
+## 新增功能
+- 1.UniformStackPanel
+    - 1.1 增加类似WrapPanel随布局大小变化
+    - 1.2 增加比例布局
+- 2.主题调整
+- 3.TreeView
+    - 3.1 增加选中时的图标   
+    - 3.2 根据不同文件适配不同的图标
+- 4.TabControl样式
+    - 4.1 TabItem选中时的样式包括轮廓、下划线
+- 5.文件拖拽读取
+
+
+## 修复Bug
+- 1.修复UniformStackPanel某个子项不可见时，重新计算布局和大小
